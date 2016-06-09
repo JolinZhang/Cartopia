c3c692476591220985777f5cf483fa5f7b4646d1
Jolin Zhang <zhangru0026@gmail.com>
2016-06-08 20:43:06 -0500

    animation of signup and logon

--- app/src/main/java/com/example/jonelezhang/cartopia/MainActivity.java
+++ app/src/main/java/com/example/jonelezhang/cartopia/MainActivity.java
@@ -1,17 +1,77 @@
 package com.example.jonelezhang.cartopia;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv;
+    private View mContentView;
+    private View mLoadingView;
+    private int mShortAnimationDuration;
+    private boolean mContentLoaded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // dynamic change between sign up and log on
+        tv = (TextView)findViewById(R.id.changeto_signup);
+        mContentView = findViewById(R.id.logon_box);
+        mLoadingView = findViewById(R.id.signup_box);
+
+        // Initially hide the content view.
+        mLoadingView.setVisibility(View.GONE);
+
+        // Retrieve and cache the system's default "short" animation time.
+        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContentLoaded = !mContentLoaded;
+                showContentOrLoadingIndicator(mContentLoaded);
+            }
+        });
     }
+    private void showContentOrLoadingIndicator(boolean contentLoaded) {
+        // Decide which view to hide and which to show.
+
+        final View showView = contentLoaded ? mLoadingView : mContentView;
+        final View hideView = contentLoaded ? mContentView : mLoadingView;
+
+        // Set the "show" view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        showView.setAlpha(0f);
+        showView.setVisibility(View.VISIBLE);
+
+
+        showView.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        // Animate the "hide" view to 0% opacity. After the animation ends, set its visibility
+        // to GONE as an optimization step (it won't participate in layout passes, etc.)
+        hideView.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        hideView.setVisibility(View.GONE);
+                    }
+                });
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
