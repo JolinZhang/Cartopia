79113290d9b33452f08f07f784891837a4cdbebc
Jolin Zhang <zhangru0026@gmail.com>
2016-06-21 21:04:56 -0500

    no message

--- app/src/main/java/com/example/jonelezhang/cartopia/Sell.java
+++ app/src/main/java/com/example/jonelezhang/cartopia/Sell.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class Sell extends ToolbarConfiguringActivity {
     //initial spinner
     private Spinner year;
     private Spinner state;
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +33,43 @@ public class Sell extends ToolbarConfiguringActivity {
         //set adapter for the year spinner
         year = (Spinner) findViewById(R.id.sell_year);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter_year = ArrayAdapter.createFromResource(this,R.array.year_array,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter_year = ArrayAdapter.createFromResource(this,R.array.year_array,R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter_year.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         year.setAdapter(adapter_year);
+        //set spinner's click listener
+        year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         //set adapter for the year spinner
         state = (Spinner) findViewById(R.id.sell_state);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter_state = ArrayAdapter.createFromResource(this,R.array.state_array,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter_state = ArrayAdapter.createFromResource(this,R.array.state_array,R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter_state.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         state.setAdapter(adapter_state);
+        //set spinner's click listener
+        state.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 
