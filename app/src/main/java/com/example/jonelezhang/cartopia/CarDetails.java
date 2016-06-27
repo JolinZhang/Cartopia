dee5203cc4da84d5e11836c801222750e1a7e6f0
Jolin Zhang <zhangru0026@gmail.com>
2016-06-26 23:36:37 -0500

    show car info in car details activity

--- app/src/main/java/com/example/jonelezhang/cartopia/CarDetails.java
+++ app/src/main/java/com/example/jonelezhang/cartopia/CarDetails.java
@@ -7,19 +7,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class CarDetails extends AppCompatActivity {
+    //widget on actionbar
+    private TextView toolbar_filter;
     //JSON
     private String url;
     private String strUrl;
     static JSONArray obj = null;
     private BuyCarItem buyCar;
+    private ArrayList<BuyCarItem> car;
     //JSON Node Names
     private static final String TAG_ID = "id";
     private static final String TAG_PICTURE = "picture";
@@ -33,20 +41,35 @@ public class CarDetails extends AppCompatActivity {
     private static final String TAG_CONTACT = "contact";
     private static final String TAG_NOTES = "notes";
     private static final String TAG_CREATEDAT = "created_at";
+    // widgets on car details
+    private  TextView carDetailsPrice;
+    private TextView carDetailsMileage;
+    private TextView carDetailsYear;
+    private TextView carDetailsMake;
+    private TextView carDetailsModel;
+    private  TextView carDetailsCity;
+    private TextView carDetailsState;
+    private TextView carDetailsContact;
+    private TextView carDetailsNotes;
+    private TextView carDetailsCreatedAt;
+    private  ImageView carDetailsImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_details);
+        //invisible filter block.
+        toolbar_filter = (TextView) findViewById(R.id.toolbar_filter);
+        toolbar_filter.setVisibility(View.INVISIBLE);
         //get car id value from buy activity
         final Intent intent = getIntent();
         String car_id = intent.getStringExtra("id");
         url = "http://cartopia.club/api/cars?id="+car_id;
         // get json data for car buy list
         new CarDetailsJSONParse().execute(url);
-
-
     }
+
+
     //use AsyncTask to run JsonParse on a different thread
     private class CarDetailsJSONParse extends AsyncTask<String, String, JSONArray> {
 
@@ -62,30 +85,61 @@ public class CarDetails extends AppCompatActivity {
             }
             return obj;
         }
+
         @Override
         protected void onPostExecute(JSONArray json) {
             //get car id value
             try {
+                // Storing  JSON item in a Variable, define an ArrayList carItems to store all cars' info.
+                car = new ArrayList<>();
                 if(json != null){
-                    JSONObject finalObject = json.getJSONObject(0);
-                    buyCar = new BuyCarItem();
-                    buyCar.setId(Integer.parseInt(finalObject.getString(TAG_ID)));
-                    buyCar.setImageResourceId(finalObject.getString(TAG_PICTURE));
-                    buyCar.setPrice(Integer.parseInt(finalObject.getString(TAG_PRICE)));
-                    buyCar.setMileage(Integer.parseInt(finalObject.getString(TAG_MILEAGE)));
-                    buyCar.setYear(Integer.parseInt(finalObject.getString(TAG_YEAR)));
-                    buyCar.setMake(finalObject.getString(TAG_MAKE));
-                    buyCar.setModel(finalObject.getString(TAG_MODEL));
-                    buyCar.setCity(finalObject.getString(TAG_CITY));
-                    buyCar.setState(finalObject.getString(TAG_STATE));
-                    buyCar.setContact(finalObject.getString(TAG_CONTACT));
-                    buyCar.setNotes(finalObject.getString(TAG_NOTES));
-                    buyCar.setCreatedAt(finalObject.getString(TAG_CREATEDAT));
+                    for(int i=0; i<json.length(); i++) {
+                        JSONObject finalObject = json.getJSONObject(i);
+                        buyCar = new BuyCarItem();
+                        buyCar.setId(Integer.parseInt(finalObject.getString(TAG_ID)));
+                        buyCar.setImageResourceId(finalObject.getString(TAG_PICTURE));
+                        buyCar.setPrice(Integer.parseInt(finalObject.getString(TAG_PRICE)));
+                        buyCar.setMileage(Integer.parseInt(finalObject.getString(TAG_MILEAGE)));
+                        buyCar.setYear(Integer.parseInt(finalObject.getString(TAG_YEAR)));
+                        buyCar.setMake(finalObject.getString(TAG_MAKE));
+                        buyCar.setModel(finalObject.getString(TAG_MODEL));
+                        buyCar.setCity(finalObject.getString(TAG_CITY));
+                        buyCar.setState(finalObject.getString(TAG_STATE));
+                        buyCar.setContact(finalObject.getString(TAG_CONTACT));
+                        buyCar.setNotes(finalObject.getString(TAG_NOTES));
+                        buyCar.setCreatedAt(finalObject.getString(TAG_CREATEDAT));
+                        car.add(buyCar);
+                    }
                 }
             }catch(JSONException e){
                 e.printStackTrace();
             }
 
+            //get all widgets on car details list card
+            carDetailsPrice = (TextView) findViewById(R.id.carDetails_price);
+            carDetailsMileage = (TextView) findViewById(R.id.carDetails_mileage);
+            carDetailsYear = (TextView) findViewById(R.id.carDetails_year);
+            carDetailsMake = (TextView) findViewById(R.id.carDetails_make);
+            carDetailsModel = (TextView) findViewById(R.id.carDetails_model);
+            carDetailsCity = (TextView) findViewById(R.id.carDetails_city);
+            carDetailsState = (TextView) findViewById(R.id.carDetails_state);
+            carDetailsContact = (TextView) findViewById(R.id.carDetails_contact);
+            carDetailsNotes = (TextView) findViewById(R.id.carDetails_note);
+            carDetailsCreatedAt = (TextView) findViewById(R.id.carDetails_createAt);
+            // initial bitmap as  null
+            carDetailsImage = (ImageView) findViewById(R.id.carDetails_image);
+            GetImage.getImage(carDetailsImage, car.get(0));
+            //set all info on car list card
+            carDetailsPrice.setText("$"+car.get(0).getPrice() + "");
+            carDetailsMileage.setText(car.get(0).getMileage() + "mi");
+            carDetailsYear.setText( car.get(0).getYear()+" ");
+            carDetailsMake.setText(car.get(0).getMake()+" ");
+            carDetailsModel.setText( car.get(0).getModel()+" ");
+            carDetailsCity.setText(car.get(0).getCity()+",");
+            carDetailsState.setText(car.get(0).getState());
+            carDetailsContact.setText(car.get(0).getContact());
+            carDetailsNotes.setText(car.get(0).getNotes());
+            carDetailsCreatedAt.setText(car.get(0).getCreatedAt());
 
         }
     }
--- app/src/main/java/com/example/jonelezhang/cartopia/GetImage.java
+++ app/src/main/java/com/example/jonelezhang/cartopia/GetImage.java
@@ -1,7 +1,53 @@
 package com.example.jonelezhang.cartopia;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Jonelezhang on 6/26/16.
  */
 public class GetImage {
+    public static void getImage(final ImageView buyImage, final BuyCarItem carItem) {
+        Bitmap output = null;
+        buyImage.setImageBitmap(output);
+        //get and set image in a separate thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        //get image bitmap inputStream
+                        String imageUrl = "http://cartopia.club/assets/user_car/" + carItem.getImageResourceId();
+                        Bitmap src = BitmapFactory.decodeStream((InputStream) new URL(imageUrl).getContent());
+
+                        //rescale bitmap size. factor is 0.5
+                        Matrix m = new Matrix();
+                        int width = src.getWidth();
+                        int height = src.getHeight();
+                        int scaledWidth = (int) (width * 0.5);
+                        int scaledHeight = (int) (height * 0.5);
+                        m.setRectToRect(new RectF(0, 0, width, height), new RectF(0, 0, scaledWidth, scaledHeight), Matrix.ScaleToFit.CENTER);
+                        Bitmap output = Bitmap.createBitmap(src, 0, 0, width, height, m, true);
+                        // set image on image view buyImage
+                        buyImage.setImageBitmap(output);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
 }
